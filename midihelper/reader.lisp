57f4f555f30b7b00cc939bff11394f229444bf78
rick <rick@porkpie.state51.co.uk>
2014-11-24 10:33:06 +0000

    fix clock event routing in midi reader

--- midihelper/reader.lisp
+++ midihelper/reader.lisp
@@ -18,23 +18,23 @@
 
 (defmacro if-gesture (event &body body)
   `(match ,event
-     ((plist :EVENT-TYPE (guard event-type (or (equal event-type :snd_seq_event_noteoff)
-                                               (equal event-type :snd_seq_event_noteon)
-                                               (equal event-type
-                                                      :snd_seq_event_controller)
-                                               (equal event-type
-                                                      :snd_seq_event_pgmchange)
-                                               (equal event-type
-                                                      :snd_seq_event_chanpress)
-                                               (equal event-type
-                                                      :snd_seq_event_pitchbend)
-                                               (equal event-type
-                                                      :snd_seq_event_control14)
-                                               (equal event-type
-                                                      :snd_seq_event_nonregparam)
-                                               (equal event-type
-                                                      :snd_seq_event_regparam))))
-      ,@body)))
+      ((plist :EVENT-TYPE (guard event-type (or (equal event-type :snd_seq_event_noteoff)
+                                                (equal event-type :snd_seq_event_noteon)
+                                                (equal event-type
+                                                       :snd_seq_event_controller)
+                                                (equal event-type
+                                                       :snd_seq_event_pgmchange)
+                                                (equal event-type
+                                                       :snd_seq_event_chanpress)
+                                                (equal event-type
+                                                       :snd_seq_event_pitchbend)
+                                                (equal event-type
+                                                       :snd_seq_event_control14)
+                                                (equal event-type
+                                                       :snd_seq_event_nonregparam)
+                                                (equal event-type
+                                                       :snd_seq_event_regparam))))
+       ,@body)))
 
 (defmacro if-clock (event &body body)
   `(match ,event
@@ -48,7 +48,7 @@
           (if-gesture mess
             (! *midi-in-chan* mess))
           (if-clock mess
-            (! *tick-chan* "tick")))))
+            (! *slave-tick-chan* "tick")))))
 
 (defvar *midi-in-thread* nil)
 
