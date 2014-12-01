99656469c6bd220b7a7ecc1ba460ff4ccabdf524
rick <rick@porkpie.state51.co.uk>
2014-12-01 18:37:49 +0000

    multiple keypress bindings are magic!

--- midihelper/slime-bindings.el
+++ midihelper/slime-bindings.el
@@ -16,6 +16,12 @@
   (interactive)
   (slime-interactive-eval "(calispel:! midihelper:*clock-ctrl-chan* (midihelper:ev-continue))"))
 
+(defun pring ()
+  (interactive)
+  (slime-interactive-eval "(print \"priiiiing!\")"))
+
 (global-set-key (kbd "M-m") 'ev-stop)
 (global-set-key (kbd "M-k") 'ev-continue)
 (global-set-key (kbd "M-l") 'ev-start)
+(global-set-key (kbd "M-n M-n") 'pring)
+(global-set-key (kbd "M-j") 'pring)
