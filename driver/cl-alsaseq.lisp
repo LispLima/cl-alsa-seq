f76739fc3b4be8ae038689859620560ccdfc1c41
rick venn <richard.venn@gmail.com>
2015-01-25 14:15:28 +0000

    attempt to refactor the struct-bothering

--- driver/cl-alsaseq.lisp
+++ driver/cl-alsaseq.lisp
@@ -34,7 +34,7 @@
   "close port <port> on alsa sequencer object <seq>"
   (snd_seq_delete_simple_port seq port))
 
-(defmacro! with-seq ((seq &key (direction :duplex) (name "Common Lisp")) &body body)
+(defmacro! with-seq ((seq &key (name "Common Lisp")) &body body)
   "open an alsa sequencer connection <seq>, named <name> with lexical scope in <body>"
   `(let* ((,g!seq (open-seq ,name))
           (,seq (mem-ref ,g!seq :pointer)))
@@ -95,27 +95,75 @@
 (defun clear-foreign-type (*ev type)
   (memset *ev 0 (foreign-type-size type)))
 
-(defmacro with-snd_seq_ev_ctrl ((var channel unused param value)
-                                &body body)
-  (declare (ignore unused))
+;; (defcvar "errno" :int)
+
+(defun set-addr-slots (addr *port *client)
+  (with-foreign-slots (((:pointer port) (:pointer client))
+                       addr (:struct snd_seq_addr_t))
+    (setf (mem-ref port :uchar) *port)
+    (setf (mem-ref client :uchar) *client )))
+
+(defmacro with-midi-event ((var data type
+                                &key (queue SND_SEQ_QUEUE_DIRECT))
+                           &body body)
   `(let ((,var (convert-to-foreign (list
-                                    'channel ,channel
-                                    'param ,param
-                                    'value ,value
+                                    'type ,type
+                                    'queue ,queue
                                     )
-                                   '(:struct snd_seq_ev_ctrl_t))))
+                                   '(:struct snd_seq_event_t))))
+     (with-foreign-slots (((:pointer data))
+                          ,var (:struct snd_seq_event_t))
+       (let ((,data data))
+         ,@body))))
+
+(defmacro! with-midi-ctrl-event ((var channel param value) &body body)
+  `(with-midi-event (,var ,g!data :snd_seq_ev_ctrl)
+     (let ((,g!channel ,channel)
+           (,g!param ,param)
+           (,g!value ,value))
+       (with-foreign-slots (((:pointer channel)
+                             (:pointer param)
+                             (:pointer value))
+                            ,g!data '(:struct snd_seq_ev_ctrl_t))
+         (setf (mem-ref channel :uchar) ,g!channel)
+         (setf (mem-ref param :uint) ,g!param)
+         (setf (mem-ref value :int) ,g!value)))
      ,@body))
 
-(defmacro with-snd_seq_ev_note ((var note velocity channel off_velocity duration)
-                                &body body)
-  `(let ((,var (convert-to-foreign (list
-                                    'note ,note
-                                    'velocity ,velocity
-                                    'channel ,channel
-                                    'off_velocity ,off_velocity
-                                    'duration ,duration
-                                    )
-                                   '(:struct snd_seq_ev_note_t))))
+(defmacro! with-midi-note-event ((var channel note velocity
+                                      off_velocity duration) &body body)
+  `(with-midi-event (,var ,g!data :snd_seq_ev_note)
+     (let ((,g!channel ,channel)
+           (,g!note ,note)
+           (,g!velocity ,velocity)
+           (,g!off_velocity ,off_velocity)
+           (,g!duration ,duration))
+       (with-foreign-slots (((:pointer channel)
+                             (:pointer note)
+                             (:pointer velocity)
+                             (:pointer off_velocity)
+                             (:pointer duration))
+                            ,g!data '(:struct snd_seq_ev_ctrl_t))
+         (setf (mem-ref channel :uchar) ,g!channel)
+         (setf (mem-ref note :uchar) ,g!note)
+         (setf (mem-ref velocity :uchar) ,g!velocity)
+         (setf (mem-ref off_velocity :uchar) ,g!off_velocity)
+         (setf (mem-ref duration :uint) ,g!duration)
+         ))
+     ,@body))
+
+(defmacro! with-midi-queue-event ((var queue unused param) &body body)
+  `(with-midi-event (,var ,g!data :snd_seq_ev_queue_control)
+     (let ((,g!queue ,queue)
+           (,g!unused ,unused)
+           (,g!param ,param))
+       (with-foreign-slots (((:pointer queue)
+                             (:pointer unused)
+                             (:pointer param))
+                            ,g!data '(:struct snd_seq_ev_queue_control_t))
+         (setf (mem-ref queue :uchar) ,g!queue)
+         (setf (mem-ref unused :pointer) ,g!unused)
+         (setf (mem-ref param :pointer) ,g!param)))
      ,@body))
 
 (defmacro with-snd_seq_ev_queue_control ((var queue unused param)
@@ -128,36 +176,11 @@
                                    '(:struct snd_seq_ev_queue_control_t))))
      ,@body))
 
-(defcvar "errno" :int)
-
-(defmacro with-midi-event ((var type &key (queue SND_SEQ_QUEUE_DIRECT)) &body body)
-  `(let ((,var (convert-to-foreign (list
-                                    'type ,type
-                                    'queue ,queue
-                                    )
-                                   '(:struct snd_seq_event_t))))
-     ,@body))
-
-(defun set-addr-slots (addr *port *client)
-  (with-foreign-slots (((:pointer port) (:pointer client))
-                       addr (:struct snd_seq_addr_t))
-    (setf (mem-ref port :uchar) *port )
-    (setf (mem-ref client :uchar) *client )))
-
-(defcfun "memcpy" :void
-  (*dest :pointer)
-  (*src :pointer)
-  (*n :uchar))
-
-(defun send-midi (*seq my-port *data note-type)
-  (with-midi-event (event (ev-key-int
-                           note-type))
-    (with-foreign-slots (((:pointer dest) (:pointer source) (:pointer data))
-                         event
-                         (:struct snd_seq_event_t))
-      (set-addr-slots source my-port 0)
-      (set-addr-slots dest SND_SEQ_ADDRESS_UNKNOWN SND_SEQ_ADDRESS_SUBSCRIBERS)
-      (memcpy data *data (foreign-type-size '(:union snd_seq_event_data))))
+(defun send-midi (*seq my-port event)
+  (with-foreign-slots (((:pointer source) (:pointer dest))
+                       event (:struct snd_seq_event_t))
+    (set-addr-slots source my-port 0)
+    (set-addr-slots dest SND_SEQ_ADDRESS_UNKNOWN SND_SEQ_ADDRESS_SUBSCRIBERS)
     (snd_seq_event_output *seq event)
     (snd_seq_drain_output *seq)))
 
@@ -170,17 +193,17 @@
 (defun send-note (velocity note channel note-type
                   *seq my-port)
   (event-type-assert note-type :SND_SEQ_EVENT_NOTE :SND_SEQ_EVENT_CONTROLLER)
-  (with-snd_seq_ev_note (data note velocity channel 0 0)
-    (send-midi *seq my-port data note-type )))
+  (with-midi-note-event (event channel note velocity 0 0)
+    (send-midi *seq my-port event)))
 
 (defun send-ctrl (channel param value ctrl-type
                   *seq my-port)
   (event-type-assert ctrl-type :SND_SEQ_EVENT_CONTROLLER :SND_SEQ_EVENT_SONGPOS)
-  (with-snd_seq_ev_ctrl (data channel (null-pointer) param value)
-    (send-midi *seq my-port data ctrl-type)))
+  (with-midi-ctrl-event (event channel param value)
+    (send-midi *seq my-port event)))
 
 (defun send-queue-ctrl (queue queue-ctrl-type
                         *seq my-port)
   (event-type-assert queue-ctrl-type :SND_SEQ_EVENT_START :SND_SEQ_EVENT_TUNE_REQUEST)
-  (with-snd_seq_ev_queue_control (data queue (null-pointer) (null-pointer))
-    (send-midi *seq my-port data queue-ctrl-type)))
+  (with-midi-queue-event (event queue (null-pointer) (null-pointer))
+    (send-midi *seq my-port event)))
